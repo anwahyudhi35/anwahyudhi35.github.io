2580845883efa407e0299c11259d6f8e0795cda4
Alvian Wahyudhi <anwahyudhi35@gmail.com>
2021-09-14 20:21:16 +0800

    tampilan 1.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--$bps)*c+358=6&e!q#h4wg1t6$+yzz4pn#p5u(hd3$u@c7uj3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
